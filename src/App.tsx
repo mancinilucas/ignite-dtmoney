f2724f539f0b353a9ef9abcafb6042f722a67bd8
mancinilucas <mancini.lucasg@gmail.com>
2021-11-15 22:01:28 -0300

    context add

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import { Dashboard } from "./components/Dashboard";
 import { Header } from "./components/Header";
 import Modal from 'react-modal'
 import { useState } from "react";
+import { TransactionsProvider } from "./TransactionsContext";
 
 import { NewTransactionModal } from "./components/NewTransactionModal";
 import { GlobalStyle } from "./styles/global";
@@ -20,7 +21,7 @@ export function App() {
   }
   
   return (
-    <>
+    <TransactionsProvider> 
       <Header onOpenNewTransactionModal={handleOpenNewTransactionModal}/>
 
       <Dashboard />
@@ -31,7 +32,7 @@ export function App() {
       />
 
       <GlobalStyle />
-    </>
+    </TransactionsProvider>
   );
 }
 
